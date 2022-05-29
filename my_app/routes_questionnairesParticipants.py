c6afc224e0ef5324c8ab6ff27f868fae69cd85d8
Rudy Kabimbi <rudykabimbingoy@Air-de-Rudy.lan>
2022-05-29 19:00:58 +0200

    formulaire demographique est OPERATIONAL

--- my_app/routes_questionnairesParticipants.py
+++ my_app/routes_questionnairesParticipants.py
@@ -16,7 +16,7 @@ import math
 
 from my_app import db
 from my_app.models.experimentation import Experimentation
-from my_app.models.participant import Participant, Sexe #import de la db
+from my_app.models.participant import Participant, Sexe, ExperienceJeei, Localisation, Experience  #import de la db
 
 from my_app.models.user import User
 
@@ -67,7 +67,7 @@ def fonction_questionnaireParticipantsDemographique(UrlUtilisateur):
     if participant:
         print("c'est bien un participant  et donc on accèpte qu'il se connecte avec cet url qui lui est propre")
         nomJEEI=jeei.nom
-        return render_template("frontend_etudiant/questionnaireParticipantsDemographique.html",currentUser=current_user,jeei=jeei,participant=participant, experimentation=experimentation,sexes=Sexe)
+        return render_template("frontend_etudiant/questionnaireParticipantsDemographique.html",currentUser=current_user,jeei=jeei,participant=participant, experimentation=experimentation,sexes=Sexe,localisations=Localisation, experiences=Experience, experiencesJeei=ExperienceJeei)
     else:
        return render_template("user_login.html")
 
@@ -79,7 +79,13 @@ def fonction_sauvegardeQuestionnaireDemographique():
     participant = Participant.query.filter_by(id=idParticipant).first()
     participant.age=request.args.get("age")
     participant.sexe=request.args.get("sexe")
+    participant.localisation= request.args.get("localisation")
+    participant.experience=request.args.get("experience")
+    participant.experienceJeei=request.args.get("experienceJeei")
     db.session.add(participant)
     db.session.commit()
-    return render_template("frontend_etudiant/remerciements.html")
+    return "ok"
 
+@app.route("/remerciements", methods=['GET', 'POST'])
+def fonction_remerciements():
+    return render_template("frontend_etudiant/remerciements.html")
--- my_app/static/js/fonctions_questionnaireParticipantsDemographique.js
+++ my_app/static/js/fonctions_questionnaireParticipantsDemographique.js
@@ -4,12 +4,15 @@ function sauverFormulaireDemographique(idParticipant, idExperimentation)
     console.log("sauverFormulaireDemographique")
     age= document.getElementById("idQuestionnaireDemographiqueAgeParticipant").value
     sexe = document.getElementById("idQuestionnaireDemographiqueSexeParticipant").value
+    localisation = document.getElementById("idQuestionnaireDemographiqueLocalisationParticipant").value
+    experience = document.getElementById("idQuestionnaireDemographiqueExperienceParticipant").value
+    experienceJeei = document.getElementById("idQuestionnaireDemographiqueExperienceJeeiParticipant").value
 
-
+    
     console.log("idParticipant "+ idParticipant+ "idExperiemnation:"+ idExperimentation+"age : "+age+"sexe"+sexe)
 
     var xhttp = new XMLHttpRequest( );
-    let url = new URL('http://127.0.0.1:5000/sauvegardeQuestionnaireDemographique?idParticipant='+ idParticipant+'&idExperimentation='+idExperimentation+'&age='+age+'&sexe='+sexe  );  
+    let url = new URL('http://127.0.0.1:5000/sauvegardeQuestionnaireDemographique?idParticipant='+ idParticipant+'&idExperimentation='+idExperimentation+'&age='+age+'&sexe='+sexe+'&localisation='+localisation+'&experience='+experience+"&experienceJeei="+experienceJeei  );  
     xhttp.open("GET", url.toString(), true);
     xhttp.send()
     xhttp.onreadystatechange = function()
@@ -25,6 +28,7 @@ function reponseSauvegardeQuestionnaireDemographique(responseText)
 {
     console.log("reponseSauvegardeQuestionnaireDemographique")
     console.log(responseText)
+    window.location.replace("http://127.0.0.1:5000/remerciements");
 
   
 }
